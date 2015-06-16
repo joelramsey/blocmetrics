a7d0d194402d6e3e6f44a1f93a23a7d5e9db20a4
joelramsey <joel.d.ramsey@gmail.com>
2015-06-16 17:15:45 +0000

    Removed JS snippet for self reporting

--- app/assets/javascripts/api/events.js
+++ app/assets/javascripts/api/events.js
@@ -1,4 +1,4 @@
-var Blocmetrics = {
+var blocmetrics = {
   report: function(name) {
    var _bm_event = {
       name: name
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
    
   namespace :api, defaults: { format: :json } do
      resources :events, only: [:create]
+     match 'events' => "events#create", via: :options, as: :events_options
    end
   root to: 'welcome#index'
 end

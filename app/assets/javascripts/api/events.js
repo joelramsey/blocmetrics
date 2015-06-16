39f64a2d12d5abcaf1cd43030e6f10f83fff2b06
joelramsey <joel.d.ramsey@gmail.com>
2015-06-16 16:59:24 +0000

    Updated JS for Eventing

---
+++ app/assets/javascripts/api/events.js
@@ -0,0 +1,18 @@
+var Blocmetrics = {
+  report: function(name) {
+   var _bm_event = {
+      name: name
+      };
+    var _bm_request = new XMLHttpRequest();
+    _bm_request.open("POST", "http://localhost:3000/api/events", true);
+    _bm_request.setRequestHeader('Content-Type', 'application/json');
+    // #5
+    _bm_request.onreadystatechange = function() {
+      // this function runs when the Ajax request changes state.
+      // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+    };
+    _bm_request.send(JSON.stringify(_bm_event));
+  }
+};
+
+// javascript_include_tag "http://localhost:3000/api/events.js"
--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,23 +1,45 @@
 class API::EventsController < ApplicationController
+  before_filter :cors_preflight_check
+  after_filter :set_headers
+  skip_before_action :verify_authenticity_token
 
-   skip_before_action :verify_authenticity_token
- 
-   def create
-     registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
-     
-     if registered_application.nil?
-       render json: "Unregistered application", status: :unprocessable_entity
-     else @event = registered_application.events.build(event_params)
-       if @event.save
-         render json: @event, status: :created
-       else
-         render json: "Missing event name", status: :unprocessable_entity
-       end
-     end
-   end
-  
+  def create
+    registered_application = RegisteredApplication.find_by(url: request.env['HTTP_ORIGIN'])
+    if registered_application.nil?
+      render json: "Unregistered application", status: :unprocessable_entity
+    else
+      @event = registered_application.events.build(event_params)
+        if @event.save
+          render json: @event, status: :created
+        else
+          render @event.errors, status: :unprocessable_entity
+        end
+    end
+  end
+
   private
-   def event_params
-     params.require(:event).permit(:name)
-   end
- end
+  def event_params
+    params.require(:event).permit(:name)
+  end
+
+  ## curl statement used that works
+  #curl -v -H "Accept: application/json" -H "Origin: http://localhost:3000" --header "Content-type: application/json" -X POST -d '{"name":"foobar8"}'  http://localhost:4000/api/events
+  
+  # Allows resources to be shared across domains -- cross-origin resource sharing
+  def set_headers
+    headers['Access-Control-Allow-Origin'] = "*"
+    headers['Access-Control-Allow-Methods'] = "POST, GET, OPTIONS"
+    headers['Access-Control-Allow-Headers'] = "Access-Control-Allow-Origin, Content-Type"
+  end
+
+  # A CORS preflight check allows poorly designd and legacy servers to support CORS by executing a sanity check
+  # between client and server
+  def cors_preflight_check
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Access-Control-Allow-Origin, X-Requested-With, X-Prototype-Version, Content-Type'
+    headers['Access-Control-Max-Age'] = '1728000'
+    render text: "" if request.method == "OPTIONS"
+  end
+
+end

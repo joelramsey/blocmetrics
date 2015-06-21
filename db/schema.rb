abb9285fd04e7c682d775554bea3fd4c496b4823
joelramsey <joel.d.ramsey@gmail.com>
2015-06-21 00:36:33 +0000

    modified views

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20150605034712) do
 
   create_table "registered_applications", force: :cascade do |t|
     t.string   "url"
-    t.string   "app_name"
+    t.string   "name"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
